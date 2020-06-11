3f5eda6e1ddb22ab40f535e2dacd692c4c618d24
cshel <sheltontechcto@gmail.com>
2020-06-11 12:26:45 -0500

    Update for TF2 and train on desktop

--- Tensorflow/new_train.py
+++ Tensorflow/new_train.py
@@ -5,6 +5,8 @@ import tensorflow
 from loss import depth_loss_function
 import os
 
+tensorflow.float16
+
 # Parameters
 
 # Allows more memory usage - Still not enough
@@ -17,7 +19,7 @@ config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 # End of memory patch
 
-batch_size = 8
+batch_size = 4
 learning_rate = 0.0001
 epochs = 10
 
@@ -37,12 +39,24 @@ checkpoint_path = "training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 cp_callback = tensorflow.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1)
 
+# Uncomment to load from existing checkpoints
+model.load_weights(checkpoint_path)
+
 # Start training
-# model.fit(train_generator, epochs=5, steps_per_epoch=dl.length // batch_size, shuffle=True, callbacks=[cp_callback])
-model.fit(train_generator, epochs=3, steps_per_epoch=100, shuffle=True, callbacks=[cp_callback])
+model.fit(train_generator, epochs=5, steps_per_epoch=dl.length // batch_size, shuffle=True, callbacks=[cp_callback])
+# model.fit(train_generator, epochs=1, steps_per_epoch=100, shuffle=True, callbacks=[cp_callback])
+
+
+def rep_data_gen():
+    for input_value in train_generator.take(100):
+        yield [input_value]
+
 
 converter = tensorflow.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tensorflow.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tensorflow.float16]
 tfl_model = converter.convert()
 
-open("./depth_trained2.tflite", "wb").write(tfl_model)
+
+open("depth_trained30_quant_f16.tflite", "wb").write(tfl_model)
 
